7f8d83b752e2707e6431da03132a872daa24bb42
Administrador <Administrador@TSP3297128.wks.internal.timbrasil.com.br>
2016-11-08 12:56:15 -0200

    Definido metodos do webservice

--- src/br/com/ftm/exemplo/ws/AlunoWeb.java
+++ src/br/com/ftm/exemplo/ws/AlunoWeb.java
@@ -1,14 +1,28 @@
 package br.com.ftm.exemplo.ws;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 @WebService
 public class AlunoWeb {
 	
+	private List<Aluno> alunos;
+	
+	public AlunoWeb(){
+		alunos = new ArrayList<>();
+	}
+	
 	@WebMethod
-	public String ola(String nome){
-		return "Olá " + nome;
+	public String novo(Aluno a){
+		alunos.add(a);
+		return "Aluno cadasatrado com sucesso!";
 	}
 
+	@WebMethod
+	public List<Aluno> listar(){
+		return alunos;
+	}
 }
