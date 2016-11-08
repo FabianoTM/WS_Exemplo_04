c9269a71778fc24e893926a3be3c5c4a2a5bb84e
Administrador <Administrador@TSP3297128.wks.internal.timbrasil.com.br>
2016-11-08 11:22:15 -0200

    Versão inicial

---
+++ src/br/com/ftm/exemplo/ws/AlunoWeb.java
@@ -0,0 +1,14 @@
+package br.com.ftm.exemplo.ws;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class AlunoWeb {
+	
+	@WebMethod
+	public String ola(String nome){
+		return "Olá " + nome;
+	}
+
+}
